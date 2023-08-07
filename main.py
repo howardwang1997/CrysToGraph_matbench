9ba43a34d9955d3853162509afd8385f96cf7e18
howardwang1997 <howardwang1997@gmail.com>
2023-08-07 23:41:26 +0800

    #

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
 from matbench.bench import MatbenchBenchmark
 
 mb = MatbenchBenchmark(autoload=False)
+mb = mb.from_preset('matbench_v0.1', 'structure')
 
 for task in mb.tasks:
     task.load()
+    classification = task.metadata['task_type']
+    name = task.dataset_name
+    input = task.metadata['input_type']
+
     for fold in task.folds:
         train_inputs, train_outputs = task.get_train_and_val_data(fold)
+        
